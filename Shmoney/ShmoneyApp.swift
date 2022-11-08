c3b3c6a99c9f13c1d733c3eae35b0cf2ba47618d
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-11-08 16:43:53 -0600

    Initial Commit

---
+++ Shmoney/ShmoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShmoneyApp.swift
+//  Shmoney
+//
+//  Created by Oliver Walsh on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ShmoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
