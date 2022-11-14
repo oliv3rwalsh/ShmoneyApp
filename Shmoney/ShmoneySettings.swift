ea16843e93a01f75e7c309ee5f6669d24d1c2136
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-11-14 16:33:35 -0600

    added part of the wage/tax editing functions

---
+++ Shmoney/ShmoneySettings.swift
@@ -0,0 +1,20 @@
+//
+//  ShmoneySettings.swift
+//  Shmoney
+//
+//  Created by Oliver Walsh on 11/14/22.
+//
+
+import SwiftUI
+
+struct ShmoneySettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShmoneySettings_Previews: PreviewProvider {
+    static var previews: some View {
+        ShmoneySettings()
+    }
+}
