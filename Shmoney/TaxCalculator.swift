8567ae4c8f0acd11f12dad5a25a0615ef1d565ca
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-11-14 12:33:59 -0600

    added navigation bar

---
+++ Shmoney/TaxCalculator.swift
@@ -0,0 +1,20 @@
+//
+//  TaxCalculator.swift
+//  Shmoney
+//
+//  Created by Oliver Walsh on 11/14/22.
+//
+
+import SwiftUI
+
+struct TaxCalculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaxCalculator_Previews: PreviewProvider {
+    static var previews: some View {
+        TaxCalculator()
+    }
+}
